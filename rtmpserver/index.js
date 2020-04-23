4dfb03c671768a658818c209f406a670a9d4d7ca
Julian Hoyos <jhoyos135@gmail.com>
2020-04-22 21:02:53 -0400

    change

--- rtmpserver/index.js
+++ rtmpserver/index.js
@@ -1,4 +1,4 @@
-const NodeMediaServer = require('node-media-server');
+const NodeMediaServer = require("node-media-server");
 
 const config = {
   rtmp: {
@@ -6,12 +6,13 @@ const config = {
     chunk_size: 60000,
     gop_cache: true,
     ping: 60,
-    ping_timeout: 30
+    ping_timeout: 30,
   },
+
   http: {
     port: 8000,
-    allow_origin: '*'
-  }
+    allow_origin: "*",
+  },
 };
 
 let nms = new NodeMediaServer(config);
